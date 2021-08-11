c56f8de2a86771e1be665fd90cbc343ea79b9e99
Alex Bartolo <abartolo.912@gmail.com>
2021-08-11 16:06:55 -0700

    filename change & matrix solution

--- birbs/maineR/tinydata_20210811.R
+++ birbs/maineR/tinydata_20210811.R
@@ -75,10 +75,32 @@ library(vegan)
 # want it to look like the BCI data here:
 data(BCI)
 
-new <- data.frame(matrix(nrow = length(maine01[ , 10:59]), 
+comms_matrix <- data.frame(matrix(nrow = length(maine01[ , 10:59]), 
                          ncol = length(maine01$AOU)))
-colnames(new) <- unique(maine01$AOU)
-head(new)
+colnames(comms_matrix) <- unique(maine01$AOU)
+head(comms_matrix)
 # looks good, now for-loop to fill...
 
 
+
+allstops_names <- colnames(maine01[10:59])
+# creating names for reference within for loop. this lets the for loop be more flexible, or at least i think.
+aou_names <- unique(maine$AOU)
+# making vector of unique AOU names for reference in for loop.
+
+for (i in 1:length(aou_names)) { 
+  
+  # does length(aout_names) need to be unique()? will eventually have to reckon with the fact that some communities might have repated AOU values... or should they? maybe they shouldn't if we take averages across years... because the repitition of AOU codes only comes with years right? check this and think about it. 
+  
+  a <- as.numeric(maine01[allstops_names][which(maine01$AOU == aou_names[i]), ])
+  comms_matrix[, which(maine01$AOU == aou_names[i])] <- a
+  
+}
+
+head(comms_matrix)
+# it worked!
+
+# check last row, another way to do it: 
+table(as.numeric(maine01[which(maine01$AOU == aou_names[43]), allstops_names]) == comms_matrix[ , 43])
+
+# as we can see, all values here are TRUE, so there's a perfect match between data from the 43rd value of aou_names in maine01 and in the comms_matrix. Any discrepancies would have resulted in a FALSE value.
