850803880e0521e1610b109b77710a75a796b58a
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-08-17 12:47:57 -0700

    Melted data together

--- test.py
+++ test.py
@@ -10,7 +10,19 @@ birb_Data = pd.read_csv(path_To_Data)
 
 print('df loaded...')
 
-# printing some basic info about the data frame
-print(birb_Data.info)
+# drop 'route_totals' column 
+birb_Data_Trimmed = birb_Data.drop(['route_totals'], axis=1)
+
+# drop duplicate of 'X' column
+df2 = birb_Data_Trimmed.head(6).drop(['ID'], axis=1)
+print(df2)
+
+df3 = df2.melt(id_vars=["X", "RouteDataID", "CountryNum","StateNum","Route", "RPID", "Year", "AOU"], 
+        var_name="Stop", 
+        value_name="Value")
+
+
+print(df3.head(6))
+
 
 print('...')
