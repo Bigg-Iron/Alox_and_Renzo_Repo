cfc3e297066dc4b4d6bb03859717d8652cf9b3e0
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2021-08-17 13:10:24 -0700

    Fixed cutting off data and added some helpful labels

--- test.py
+++ test.py
@@ -6,23 +6,25 @@ import os
 
 path_To_Data = pathlib.PurePath('data/maine.csv')
 # Read data into Pandas data frame
-birb_Data = pd.read_csv(path_To_Data)
+maineCSV = pd.read_csv(path_To_Data)
 
 print('df loaded...')
 
-# drop 'route_totals' column 
-birb_Data_Trimmed = birb_Data.drop(['route_totals'], axis=1)
+# drop 'route_totals' column. You can get totals using pandas.DataFrame.sum
+maineCSV_Trimmed = maineCSV.drop(['route_totals'], axis=1)
 
 # drop duplicate of 'X' column
-df2 = birb_Data_Trimmed.head(6).drop(['ID'], axis=1)
-print(df2)
+df2 = maineCSV_Trimmed.drop(['duplicateOf_X'], axis=1)
+
+# Uncomment the line below to check the first 5 instances of the data frame
+# print(df2.head(6))
 
 df3 = df2.melt(id_vars=["X", "RouteDataID", "CountryNum","StateNum","Route", "RPID", "Year", "AOU"], 
         var_name="Stop", 
-        value_name="Value")
+        value_name="Instances of AOU")
 
 
-print(df3.head(6))
+print(df3)
 
 
-print('...')
+print('...Done...')
